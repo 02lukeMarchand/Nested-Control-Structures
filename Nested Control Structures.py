11b92ed489d29b31a12b2672a47549fd87b25e9c
Luke Marchand <02luke.marchand@gulllakecs.org>
2019-10-23 13:56:48 -0400

    Initial Commit For Loop + While Loop

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -11,3 +11,16 @@ for i in range (3):
     print ("Outer for loop: " + str(i))
     for l in range (2):
         print ("     Inner For Loop: " + str(l))
+
+print("\n_______________________\n\n")
+
+'''
+Programmer: Luke Marchand
+Date: 10.23.19
+Program: For Loop + While Loop
+
+This program will create a for loop with a while loop embedded into it
+'''
+
+
+
